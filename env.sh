f7f784bfad7ccc6733703907f1c6bc1179524ebe
Billah, Tashrif <tbillah@bwh.harvard.edu>
2021-10-29 17:28:30 -0400

    bash environment

---
+++ env.sh
@@ -0,0 +1,5 @@
+module load CUDA/9.1.85
+source /data/pnl/soft/pnlpipe3/miniconda3/bin/activate
+conda activate hd-bet
+export PYTHONPATH=/data/pnl/soft/pnlpipe3/HD-BET:$PYTHONPATH
+export PATH=/data/pnl/soft/pnlpipe3/HD-BET/HD_BET:$PATH
